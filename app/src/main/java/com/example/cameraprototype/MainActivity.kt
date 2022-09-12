cba67861c7fff75b0f8af7ab6363ac2fb373cafc
Tanvir <rahman.tanvir.ovi@gmail.com>
2022-09-12 16:41:48 +0200

    initiating new project and implementing basic app ui

--- app/src/main/java/com/example/cameraprototype/MainActivity.kt
+++ app/src/main/java/com/example/cameraprototype/MainActivity.kt
@@ -1,11 +1,51 @@
 package com.example.cameraprototype
 
+import android.content.pm.PackageManager
+import android.hardware.camera2.CameraManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var cameraManager: CameraManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (allPermissionsGranted()) {
+//            cameraManager.startCamera()
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                REQUIRED_PERMISSIONS,
+                REQUEST_CODE_PERMISSIONS
+            )
+        }
+
     }
+    private fun createCameraManager() {
+//        cameraManager = CameraManager(
+//            this,
+//            binding.previewViewFinder,
+//            this,
+//            binding.graphicOverlayFinder
+//        )
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    companion object {
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS = arrayOf(
+            android.Manifest.permission.CAMERA,
+            android.Manifest.permission.READ_EXTERNAL_STORAGE,
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+    }
+
 }
